cffd41bc2987d2215ae9da283cd8c2c80d2c6c71
Moon3301 <carlos_db_16@live.cl>
2021-06-05 19:31:19 -0400

    commit

--- entorno001/proyecto/tienda/urls.py
+++ entorno001/proyecto/tienda/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path 
-from .views import inicio
+from .views import inicio,listadoProducto, crearProducto, modificarProducto, eliminarProducto
 
 urlpatterns = [
     path('',inicio, name= "inicio"),
+    path('inicio', inicio, name="inicio"),
+    path('listadoProducto', listadoProducto, name="listadoProducto"),
+    path('crearProducto', crearProducto, name="crearProducto"),
+    path('modificarProducto/<id>', modificarProducto, name="modificarProducto"),
+    path('eliminarProducto/<id>', eliminarProducto, name="eliminarProducto"),
     
+
     
 
 
--- entorno001/proyecto/tienda/views.py
+++ entorno001/proyecto/tienda/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render, redirect
+from .models import Producto
+from .forms import ProductoForm
 
 # Create your views here.
 
 def inicio (request):
     return render (request, 'Index.html')
+
+def listadoProducto (request):
+    listado = Producto.objects.all()
+
+    contexto = {'listado' : listado, 'user' : 'alguien'}
+
+    return render(request, 'ListadoProducto.html', contexto)
+
+def crearProducto (request):
+    contexto = {'formulario': ProductoForm}
+
+    if request.method == 'POST':
+        producto = ProductoForm(request.POST)
+        producto.save()# insert
+        contexto['mensaje'] = 'Datos guardados'
+    return render(request, 'RegistroProducto.html', contexto)
+
+def modificarProducto(request, id):
+
+    producto = Producto.objects.get(id = id)
+
+    contexto = {'formulario': ProductoForm(instance=producto) }
+
+    if request.method == 'POST' :
+        formulario = ProductoForm(data=request.POST, instance=producto)
+        formulario.save()
+        contexto = {'formulario' : ProductoForm(instance=Producto.objects.get(id = id)) }
+        contexto['mensaje'] = 'Los datos fueron guardados'
+        
+    return render(request, 'modificarProducto.html', contexto)
+
+
+def eliminarProducto(request, id ):
+    producto = Producto.objects.get(id = id)
+    producto.delete()
+    return redirect(to = "listadoProducto")
